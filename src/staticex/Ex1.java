06ce99e7d80503c99a1a9aee3734f1ced6d094bd
takeover-da <dlslyn@naver.com>
2024-07-09 17:01:55 +0900

    7.9 static Ex1추가

--- src/staticex/Ex1.java
+++ src/staticex/Ex1.java
@@ -6,7 +6,13 @@ public class Ex1 {
 
 		Student student1 = new Student();
 		student1.studentName = "둘리";
+		System.out.println(student1.studentName + " 학번: " + student1.studentID);
 		
+		Student student2 = new Student();
+		student2.studentName = "또치";
+		System.out.println(student2.studentName + " 학번: " + student2.studentID);
+		
+		System.out.println(Student.count);
 	}
 
 }
